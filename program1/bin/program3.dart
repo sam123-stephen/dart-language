2dc18e15bc78b63faa99fd02c1dd132adb32aa1c
sam123-stephen <samstephen824@gmail.com>
2025-03-19 11:31:07 +0530

    created program3

---
+++ program1/bin/program3.dart
@@ -0,0 +1,13 @@
+//final and const
+void main() {
+  final String name = "samstephen";
+  print(name);
+  //re-intialization is not allowed
+  // name="top";
+
+  //const
+  const pi = 3.14;
+  print(pi);
+  //re-intialization is not allowed
+  //pi=3.15;
+}
