d5ea819a12fd2456785de6d2c5bef8982c5b4891
sam123-stephen <samstephen824@gmail.com>
2025-03-19 11:53:40 +0530

    created program5

---
+++ program1/bin/program5.dart
@@ -0,0 +1,23 @@
+//optional parameter has three types they are, 1) NAMED PARAMETER 2)POSITIONAL PARAMETER 3)DEFAULT PARAMETER
+void main() {
+  //function
+  // person("samstephen",213332);//it is wrong way
+  //named parameteer
+  person(name: "samstephen", rollno: 213332);
+
+  //positional parameter
+
+  position("sam", 21);
+}
+
+//named parameter
+void person({String name = "", int rollno = 0}) {
+  print(name);
+  print(rollno);
+}
+
+//postional parameter
+void position(String name, int age) {
+  print(name);
+  print(age);
+}
